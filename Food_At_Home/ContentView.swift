97f0f108b58e40221358bb3410012c6593e97536
rad-hi7 <53797949+rad-hi7@users.noreply.github.com>
2025-01-25 04:48:19 -0500

    Initial Commit

---
+++ Food_At_Home/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food_At_Home
+//
+//  Created by hi on 1/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food_At_Home/Food_At_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_At_HomeApp.swift
+//  Food_At_Home
+//
+//  Created by hi on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Food_At_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
